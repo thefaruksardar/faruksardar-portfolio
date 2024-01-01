3be9f3f52c21a0b2a817b879bac26986acae01d6
faruk <thefaruksardar@gmail.com>
2024-01-01 19:12:36 +0530

    change to email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <contact@faruksardar.com>",
-      to: "faruksardar960@gmail.com",
+      to: "thefaruksardar@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
